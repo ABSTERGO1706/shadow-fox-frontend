94075914f247bdc7aa8c0a24b0f1ef9d30b11c5e
Hari Roshan <hariroshan17@gmail.com>
2026-03-23 22:06:16 +0530

    Update Squad.jsx

--- src/Squad.jsx
+++ src/Squad.jsx
@@ -6,7 +6,7 @@ function Squad()
     const [data, useData] = useState([]);
     
       useEffect(() => {
-        fetch("http://localhost:8080/players")
+        fetch("https://shadow-fox-production.up.railway.app/players")
           .then(res => {
             if (!res.ok) throw new Error('Fetch Failed');
             return res.json();
@@ -24,7 +24,7 @@ function Squad()
                         {
                             data.map(item=>(
                                 <div key={item.id} className='card-section'>
-                                    <img src={`http://localhost:8080/images/${item.imageUrl}`} alt="player" />
+                                    <img src={`https://shadow-fox-production.up.railway.app/images/${item.imageUrl}`} alt="player" />
                                     <h1>{item.name}</h1> 
                                     <div className='card-content-left'>
                                         <h4>Role</h4>
